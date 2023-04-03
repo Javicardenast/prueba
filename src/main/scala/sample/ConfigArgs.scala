1ac201d9b4106ed649c7d1d224742b7707858b7b
lct638 <lct638@inlumine.ual.es>
2023-04-03 11:21:29 +0200

    Commit de prueba

---
+++ src/main/scala/sample/ConfigArgs.scala
@@ -0,0 +1,5 @@
+package sample
+
+case class ConfigArgs() {
+
+}
--- src/main/scala/sample/main.scala
+++ src/main/scala/sample/main.scala
@@ -1,5 +1,36 @@
 package sample
 
+import org.apache.log4j.BasicConfigurator
+import org.apache.spark.sql._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+
+import java.io.File
+//import spark.implicits._
+
 object main {
-  println("Hola mundo")
+
+  val warehouseLocation = new File("spark-warehouse").getAbsolutePath
+
+  val spark = SparkSession.builder()
+    .appName("main")
+    .config("spark.sql.warehouse.dir", warehouseLocation)
+    .enableHiveSupport()
+    .getOrCreate()
+
+  val schema = StructType(List(
+    StructField("ID",IntegerType,true),
+    StructField("contrato",StringType,true),
+    StructField("MENSAJE_HUMANO",StringType,true)))
+
+  val logERROR = spark.createDataFrame(spark.sparkContext.emptyRDD[Row],schema)
+
+  def main(args: Array[String]): Unit ={
+    BasicConfigurator.configure()
+    val cfg = new ConfigArgs()
+    try{
+      cfg.parse(args)
+    }
+
+
+  }
 }
